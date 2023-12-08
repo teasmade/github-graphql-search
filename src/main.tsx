098dd4f499a4e91f3781511c1bda8672cb63af5e
Thomas Allen <thomasedwardallen@gmail.com>
2023-12-08 12:38:33 +0100

    graphql: setup apollo client with github auth

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import client from './apollo'
+import { ApolloProvider } from "@apollo/client"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>    
   </React.StrictMode>,
 )
