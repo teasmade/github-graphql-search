f7a30b583f0a0967fec78bcf14947713e5934433
Thomas Allen <thomasedwardallen@gmail.com>
2023-12-08 16:50:19 +0100

    materialUI: font imports, cssbaseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import client from './apollo'
-import { ApolloProvider } from "@apollo/client"
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CssBaseline from '@mui/material/CssBaseline';
+import App from './App.tsx';
+import './index.css';
+import client from './apollo';
+import { ApolloProvider } from '@apollo/client';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <CssBaseline />
     <ApolloProvider client={client}>
       <App />
-    </ApolloProvider>    
-  </React.StrictMode>,
-)
+    </ApolloProvider>
+  </React.StrictMode>
+);
