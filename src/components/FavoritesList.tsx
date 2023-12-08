fe42f907afe86fda3c2a6f887b14979a6b0deff6
Thomas Allen <thomasedwardallen@gmail.com>
2023-12-08 14:51:57 +0100

    rating: add test rating interface to favorites list

--- src/components/FavoritesList.tsx
+++ src/components/FavoritesList.tsx
@@ -2,20 +2,37 @@ import React from 'react';
 import { useRepos } from '../contexts/RepoContext';
 
 const FavoritesList: React.FC = () => {
-  const { favorites, removeFavorite } = useRepos();
+  const { favorites, removeFavorite, updateRating } = useRepos();
 
   return (
     <div>
       <h2>Favorites</h2>
       {favorites.length > 0 ? (
         <ul>
-          {favorites.map(repo => (
+          {favorites.map((repo) => (
             <li key={repo.id}>
-              <a href={repo.url} target="_blank" rel="noopener noreferrer">
+              <a
+                href={repo.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 {repo.name}
               </a>
               {' - '}
               {repo.stargazerCount} stars
+              <div>
+                {[1, 2, 3, 4, 5].map((rating) => (
+                  <button
+                    key={rating}
+                    style={{
+                      fontWeight: repo.rating === rating ? 'bold' : 'normal',
+                    }}
+                    onClick={() => updateRating(repo.id, rating)}
+                  >
+                    {rating}
+                  </button>
+                ))}
+              </div>
               <button onClick={() => removeFavorite(repo.id)}>
                 Remove from Favorites
               </button>
