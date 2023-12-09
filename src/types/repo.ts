260334c1c2216dfbd6cd745149ac4ecaa54ce27f
Thomas Allen <thomasedwardallen@gmail.com>
2023-12-09 10:31:41 +0100

    types: extend repo with description, topics, languages

--- src/types/repo.ts
+++ src/types/repo.ts
@@ -2,6 +2,13 @@ export type Repo = {
   id: string;
   name: string;
   url: string;
+  description?: string;
   stargazerCount: number;
+  repositoryTopics: {
+    nodes: Array<{ topic: { name: string } }>;
+  };
+  languages: {
+    nodes: Array<{ name: string }>;
+  };
   rating?: number;
 };
