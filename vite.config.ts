fa19d2222678c2bc2ae7ea4a16cd0888d8ea461b
Thomas Allen <thomasedwardallen@gmail.com>
2023-12-10 15:18:06 +0100

    linting: ignore no-unused for defineConfig in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
